431e98fd2cb8c2c26f4b1f7476550cc2c3e2e9d3
MatviiTorop <mattor264@gmail.com>
2025-02-18 21:12:58 +0200

    Create default repository interface and repository

---
+++ PlaceOfOblivion.Server/Repositories/Implementations/Repository.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlaceOfOblivion.Server.Data;
+using PlaceOfOblivion.Server.Repositories.Interfaces;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace PlaceOfOblivion.Server.Repositories.Implementations
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly POODbContext _context;
+
+        public Repository(POODbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+        public async Task<T?> FindOneAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteWhereAsync(Expression<Func<T, bool>> predicate)
+        {
+            var entities = await _context.Set<T>().Where(predicate).ToListAsync();
+            if (entities.Any())
+            {
+                _context.Set<T>().RemoveRange(entities);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T?> GetWithIncludesAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().AnyAsync(predicate);
+        }
+    }
+}
---
+++ PlaceOfOblivion.Server/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace PlaceOfOblivion.Server.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<T?> FindOneAsync(Expression<Func<T, bool>> predicate);
+        Task DeleteWhereAsync(Expression<Func<T, bool>> predicate);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    }
+}
